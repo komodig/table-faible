e145a272f98e1888188b4a70a0f58d51d5f4f683
Lutz Ballaschke <l.ballaschke@tastybyte.de>
2023-10-07 19:38:47 +0200

    apply clean code on all python modules found in directory

--- clean-code.sh
+++ clean-code.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-MODULES="mappings"
+MODULES=`ls *.py`
 
 source venv/bin/activate
 
 for mod in $MODULES; do
-  mod_=$mod.py
-  echo "$mod_"
-  python3 -m flake8 --statistics --count $mod_
-  python3 -m isort --check-only $mod_
-  python3 -m isort $mod_
+  echo "$mod"
+  python3 -m flake8 --statistics --count $mod
+  python3 -m isort --check-only $mod
+  python3 -m isort $mod
 done
